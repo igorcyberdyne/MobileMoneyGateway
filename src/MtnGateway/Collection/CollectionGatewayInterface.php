73938640de99a7389d5a5782088082814c20587c
igorcyberdyne <ekolo.openeducation@gmail.com>
2024-01-06 01:05:53 +0100

    Add PHP Doc in interface methods

--- src/MtnGateway/Collection/CollectionGatewayInterface.php
+++ src/MtnGateway/Collection/CollectionGatewayInterface.php
@@ -3,16 +3,62 @@
 namespace Ekolotech\MoMoGateway\MtnGateway\Collection;
 
 use Ekolotech\MoMoGateway\Dto\CollectRequestBody;
+use Ekolotech\MoMoGateway\Exception\AccountHolderException;
+use Ekolotech\MoMoGateway\Exception\BalanceException;
+use Ekolotech\MoMoGateway\Exception\CollectionException;
+use Ekolotech\MoMoGateway\Exception\MtnAccessKeyException;
+use Ekolotech\MoMoGateway\Exception\RefreshAccessException;
+use Ekolotech\MoMoGateway\Exception\TokenCreationException;
+use Ekolotech\MoMoGateway\Exception\TransactionReferenceException;
 
 interface CollectionGatewayInterface
 {
+    /**
+     * @param CollectRequestBody $collectRequestBody
+     * @return bool
+     * @throws CollectionException
+     * @throws MtnAccessKeyException
+     * @throws TokenCreationException
+     * @throws RefreshAccessException
+     */
     public function collect(CollectRequestBody $collectRequestBody): bool;
 
+    /**
+     * @param string $reference
+     * @return array
+     * @throws MtnAccessKeyException
+     * @throws TokenCreationException
+     * @throws TransactionReferenceException
+     * @throws RefreshAccessException
+     * @throws CollectionException
+     */
     public function collectReference(string $reference): array;
 
+    /**
+     * @throws TokenCreationException
+     * @throws BalanceException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     */
     public function balance(): array;
 
+    /**
+     * @param string $number
+     * @return bool
+     * @throws AccountHolderException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     */
     public function isAccountIsActive(string $number): bool;
 
+    /**
+     * @param string $number
+     * @return array
+     * @throws AccountHolderException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     */
     public function getAccountBasicInfo(string $number): array;
 }
--- src/MtnGateway/Disbursement/DisbursementGatewayInterface.php
+++ src/MtnGateway/Disbursement/DisbursementGatewayInterface.php
@@ -3,16 +3,63 @@
 namespace Ekolotech\MoMoGateway\MtnGateway\Disbursement;
 
 use Ekolotech\MoMoGateway\Dto\DisburseRequestBody;
+use Ekolotech\MoMoGateway\Exception\AccountHolderException;
+use Ekolotech\MoMoGateway\Exception\BalanceException;
+use Ekolotech\MoMoGateway\Exception\DisbursementException;
+use Ekolotech\MoMoGateway\Exception\MtnAccessKeyException;
+use Ekolotech\MoMoGateway\Exception\RefreshAccessException;
+use Ekolotech\MoMoGateway\Exception\TokenCreationException;
+use Ekolotech\MoMoGateway\Exception\TransactionReferenceException;
 
 interface DisbursementGatewayInterface
 {
+    /**
+     * @param DisburseRequestBody $disburseRequestBody
+     * @return bool
+     * @throws DisbursementException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     */
     public function disburse(DisburseRequestBody $disburseRequestBody): bool;
 
+    /**
+     * @param string $reference
+     * @return array
+     * @throws DisbursementException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     * @throws TransactionReferenceException
+     */
     public function disburseReference(string $reference): array;
 
+    /**
+     * @return array
+     * @throws BalanceException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     */
     public function balance(): array;
 
+    /**
+     * @param string $number
+     * @return bool
+     * @throws AccountHolderException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     */
     public function isAccountIsActive(string $number): bool;
 
+    /**
+     * @param string $number
+     * @return array
+     * @throws AccountHolderException
+     * @throws MtnAccessKeyException
+     * @throws RefreshAccessException
+     * @throws TokenCreationException
+     */
     public function getAccountBasicInfo(string $number): array;
 }
